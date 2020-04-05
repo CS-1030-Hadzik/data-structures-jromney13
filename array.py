4ea3702ed7bf0da851298dd182296588f318466d
Josh Romney <jromney13@gmail.com>
2020-04-04 21:00:59 -0600

    Final commit

---
+++ array.py
@@ -0,0 +1,18 @@
+print('Josh Romney')
+cars = ['Ford', 'Chrysler', 'Dodge', 'Ram', 'Jeep', 'Chevy', 'GMC']
+print(cars)
+print(len(cars))
+cars.append('Buick')
+print(cars)
+print(cars[3])
+cars.insert(2, 'Toyota')
+print(cars)
+cars.pop(6)
+print(cars)
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+my_array_length = len(cars)
+array_string = 'The length of my array is '
+print(array_string + str(my_array_length))
